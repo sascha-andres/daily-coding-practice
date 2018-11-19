57f027daeb390a93e5f8893b41eb3fde074dffb6
Sascha Andres <s.andres@syzygy.de>
2018-11-19 16:15:25 +0100

    feat: implement solution

--- dcp114.go
+++ dcp114.go
@@ -1,7 +1,70 @@
 package dcp114
 
+import (
+	"strings"
+)
+
 // Run executes the algorithm and returns the result
-func Run(input string, separators []string) string {
-	_ = separators
-	return input
+func Run(input string, separators string) string {
+	values := split(input, separators)
+
+	if len(values) == 1 {
+		return input
+	}
+
+	startBack := len(values) - 1
+
+	for i, value := range values {
+		//;fmt.Printf("value := %s\n", value)
+		if "" != value && strings.Contains(separators, value) {
+			continue
+		}
+
+		memorize := value
+		for fromBack := startBack; fromBack > i; fromBack-- {
+			startBack--
+			if strings.Contains(separators, values[fromBack]) {
+				continue
+			}
+			values[i] = values[fromBack]
+			values[fromBack] = memorize
+			break
+		}
+	}
+	return strings.Join(values, "")
+}
+
+// split  breaks string into pieces giving each separator a spot
+func split(input string, separators string) []string {
+	var values []string
+	value := ""
+	lastOneSeparator := false
+
+	for _, r := range input {
+		character := string(r)
+		if strings.Contains(separators, character) {
+			value, values, lastOneSeparator = handleSeparator(value, values, lastOneSeparator, character)
+		} else {
+			value = value + string(r)
+			lastOneSeparator = false
+		}
+	}
+	if value != "" {
+		values = append(values, value)
+	}
+
+	return values
+}
+
+func handleSeparator(value string, values []string, lastOneSeparator bool, character string) (string, []string, bool) {
+	if len(value) > 0 {
+		values = append(values, value)
+	}
+	if lastOneSeparator {
+		values = append(values, "")
+	}
+	values = append(values, character)
+	value = ""
+	lastOneSeparator = true
+	return value, values, lastOneSeparator
 }
--- dcp114_test.go
+++ dcp114_test.go
@@ -1,24 +1,45 @@
 package dcp114
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNoSplit(t *testing.T) {
+	const input = "hello"
+	const output = "hello"
+	const separators = ":/"
+	runTest(input, separators, output, t)
+}
 
 func TestHelloWorldHere(t *testing.T) {
-	result := Run("hello/world:here", []string{":", "/"})
-	if result != "here/world:hello" {
-		t.Fail()
-	}
+	const input = "hello/world:here"
+	const output = "here/world:hello"
+	const separators = ":/"
+	runTest(input, separators, output, t)
 }
 
 func TestTrailingDeliminator(t *testing.T) {
-	result := Run("hello/world:here/", []string{":", "/"})
-	if result != "here/world:hello/" {
-		t.Fail()
-	}
+	const input = "hello/world:here/"
+	const output = "here/world:hello/"
+	const separators = ":/"
+	runTest(input, separators, output, t)
 }
 
 func TestEmptyValue(t *testing.T) {
-	result := Run("hello//world:here/", []string{":", "/"})
-	if result != "here/world/:hello" {
+	const input = "hello//world:here/"
+	const output = "here/world/:hello/"
+	const separators = ":/"
+	runTest(input, separators, output, t)
+}
+
+/////////////////////////////////////////////////////////////////
+
+func runTest(input string, separators string, output string, t *testing.T) {
+	fmt.Printf("input := %s - output := %s separators := %s\n", input, output, separators)
+	result := Run(input, separators)
+	fmt.Println(result)
+	if result != output {
 		t.Fail()
 	}
 }
